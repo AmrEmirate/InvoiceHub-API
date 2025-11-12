7b19bdb01e31abb1c0f5c7df867c3fcb344b2bd1
Amr Emirate <amremirate03@gmail.com>
2025-11-12 09:35:50 +0700

    memperbaiki nodeimer

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,9 +1,20 @@
 import nodemailer from "nodemailer";
+import logger from "../utils/logger";
 
 export const transport = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false, // true untuk port 465, false untuk port lain seperti 587
   auth: {
-    user: process.env.MAIL_SENDER,
-    pass: process.env.MAIL_PASSWORD,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
+});
+
+transport.verify((error, success) => {
+  if (error) {
+    logger.error("Nodemailer transport error:", error);
+  } else {
+    logger.info("Nodemailer transport is ready.");
+  }
 });
