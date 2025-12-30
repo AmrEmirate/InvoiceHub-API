60a58bcd2854161fde8b207cce804f82649f3d67
Amr Emirate <amremirate03@gmail.com>
2025-12-30 17:00:13 +0700

    fix(api): make rate limit configurable via env

--- src/app.ts
+++ src/app.ts
@@ -27,10 +27,10 @@ class App {
     this.app.use(helmet());
 
     const limiter = rateLimit({
-      windowMs: 15 * 60 * 1000,
-      max: 100, 
-      standardHeaders: true, 
-      legacyHeaders: false, 
+      windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS || "900000"), // 15 minutes
+      max: parseInt(process.env.RATE_LIMIT_MAX || "100"),
+      standardHeaders: true,
+      legacyHeaders: false,
       message:
         "Too many requests from this IP, please try again after 15 minutes",
     });
